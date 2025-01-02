71711b34073b5103646645a1e67a8f9863545a66
min <jeongmin0324@gmail.com>
2025-01-02 13:45:59 +0900

    Add project settings

--- snap_booking/settings.py
+++ snap_booking/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'snap_booking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'keevo',
+        'USER': 'keevo',
+        'PASSWORD': 'keevo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
